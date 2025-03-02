c4627eaed60a948975ff96240cfa76513b33c9e0
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2025-03-02 21:46:20 +0530

    initial get request setup

---
+++ src/main/java/com/Restapitest/RestapiTest/JobRestController.java
@@ -0,0 +1,23 @@
+package com.Restapitest.RestapiTest;
+
+import com.Restapitest.RestapiTest.model.JobPost;
+import com.Restapitest.RestapiTest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class JobRestController {
+
+    @Autowired
+    public JobService service;
+
+    @GetMapping("JobPost")
+    public List<JobPost> getAlljobs(){
+        return service.getAllJobs();
+    }
+}
